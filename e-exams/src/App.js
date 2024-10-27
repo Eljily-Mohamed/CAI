d1021665fc21544026540cf97690acd8058e1367
Eljily-Mohamed <hamadekj15@gmail.com>
2024-10-27 18:18:00 +0100

    add

--- e-exams/src/App.js
+++ e-exams/src/App.js
@@ -1,9 +1,11 @@
+// src/App.js
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// Import the AuthProvider
+// Import the AuthProvider and ExamAccessProvider
 import { AuthProvider } from "./contexts/AuthContext";
+import { ExamAccessProvider } from "./contexts/ExamAccessContext";
 
 // Router pages
 import { Home } from "./pages/Home";
@@ -14,24 +16,28 @@ import { Contact } from "./pages/Contact";
 import { Signup } from "./pages/Signup";
 import { PreExam } from "./pages/PreExam";
 import { Profile } from "./pages/Profile";
-// import NotFound from "./pages/NotFound"; // Import NotFound component
+//import NotFound from "./pages/NotFound"; // Import NotFound component
+import PrivacyPolicy from './components/PrivacyPolicy';
 
 function App() {
   return (
     <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/Home" element={<Home />} />
-          <Route path="/Stats" element={<Statistics />} />
-          <Route path="/Contact" element={<Contact />} />
-          <Route path="/" element={<Auth />} />
-          <Route path="/Signup" element={<Signup />} />
-          <Route path="/Pre-exam" element={<PreExam />} />
-          <Route path="/Exam" element={<Exam />} />
-          <Route path="/Profile" element={<Profile />} />
-          {/* <Route path="*" element={<NotFound />} /> Catch all unmatched routes */}
-        </Routes>
-      </Router>
+      <ExamAccessProvider>
+        <Router>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/stats" element={<Statistics />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/" element={<Auth />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/pre-exam" element={<PreExam />} />
+            <Route path="/exam/:id" element={<Exam />} />{" "}
+            {/* Route avec paramètre d'ID */}
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          </Routes>
+        </Router>
+      </ExamAccessProvider>
     </AuthProvider>
   );
 }
